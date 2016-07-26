30e4acd76c93ad1a80bb120ad8d6660f9c3786ab
merkelbe <merkelbe@mail.uc.edu>
2016-07-25 23:11:54 -0500

    Code Refactor
    Moved board game input logic to the board game class instead of the game
    class.  Made almost all board game methods private so there's a limited
    interface between the game and the game board.

---
+++ Board Game/Board Game/Board_Game/Globals.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BoardGame
+{
+    static class g
+    {
+        internal enum InputType { Nothing, LeftClick, LeftDoubleClick, RightClick, RightDoubleClick };
+
+
+    }
+}
